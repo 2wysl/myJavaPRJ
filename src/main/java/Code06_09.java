1fb178cbe949f08b458bed5439fb9cd64e365d7a
2wysl <jjw7380@naver.com>
2024-04-15 15:14:06 +0900

    240415

---
+++ src/main/java/Code06_09.java
@@ -0,0 +1,20 @@
+public class Code06_09 {
+    public static void main(String[] args) {
+
+//        for(int i=0; i<3; i++){
+//            for(int k=0; k<2; k++){
+//                System.out.println("자바는 안 쉽습니다.^^"+"(i: "+i+", k:"+k+")");
+//            }
+//
+//            System.out.println(i+"번째 i 반복문 종료");
+//        }
+
+        for(int i=2; i<=9; i++){
+            for(int j=1; j<=9; j++){
+                System.out.println(i+" x "+j+" = "+i*j);
+
+            }
+//            System.out.print("\n");
+        }
+    }
+}
