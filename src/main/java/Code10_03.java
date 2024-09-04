5a35060f1a8f7b886b43bd16f3a74561bd1acc8e
2wysl <jjw7380@naver.com>
2024-09-04 15:43:42 +0900

    240904

---
+++ src/main/java/Code10_03.java
@@ -0,0 +1,14 @@
+//public class Code10_3 {
+//    public static int plus(int v1, int v2) { // 습관적으로 접근지정자 쓰기, static or new(는 무조건 클래스)로 메모리 올리기
+//        // static 안쓰면 10행에 Code10_2 a=new Code10_3(); 추가 & 11행을 hap=a.plus(100, 200);으로 수정
+//        int result;
+//        result = v1 + v2;
+//        return result;
+//    }
+//
+//    public static void main(String[] args) {
+//        int hap;
+//        hap = plus(100, 200);
+//        System.out.println("100과 200의 plus() 메서드 결과 :" + hap);
+//    }
+//}
